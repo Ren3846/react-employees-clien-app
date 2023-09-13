cff9ed60d3d7dcaec5f1bcb2920265e22ce1b424
Renat Chernov <renat3846@gmail.com>
2023-09-13 12:16:53 +0300

    commit

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,22 @@
-import { Layout, Space, Typography } from "antd";
-import { TeamOutlined, UserOutlined, LoginOutlined } from "@ant-design/icons";
-import styles from "./index.module.css";
-import { Link, useNavigate } from "react-router-dom";
-import { Path } from "../../paths";
-import CustomButton from "../button";
-import { useDispatch, useSelector } from "react-redux";
-import { logout, selectUser } from "../../features/auth/authSlice";
+import { Layout, Space, Typography } from 'antd'
+import { TeamOutlined, UserOutlined, LoginOutlined } from '@ant-design/icons'
+import styles from './index.module.css'
+import { Link, useNavigate } from 'react-router-dom'
+import { Path } from '../../paths'
+import CustomButton from '../button'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout, selectUser } from '../../features/auth/authSlice'
 
 const Header = () => {
-  const user = useSelector(selectUser);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const user = useSelector(selectUser)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const onLogoutClick = () => {
-    dispatch(logout());
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
+    dispatch(logout())
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
 
   return (
     <Layout.Header className={styles.header}>
@@ -29,32 +29,28 @@ const Header = () => {
 
       <Space className={styles.rightButtons}>
         {user ? (
-          <CustomButton
-            type="text"
-            icon={<LoginOutlined />}
-            onClick={onLogoutClick}
-          >
-            {" "}
+          <CustomButton type='text' icon={<LoginOutlined />} onClick={onLogoutClick}>
+            {' '}
             Вийти
           </CustomButton>
         ) : (
           <Link to={Path.login}>
-            <CustomButton type="text" icon={<LoginOutlined />}>
-              {" "}
-              Вхід{" "}
+            <CustomButton type='text' icon={<LoginOutlined />}>
+              {' '}
+              Вхід{' '}
             </CustomButton>
           </Link>
         )}
 
         <Link to={Path.register}>
-          <CustomButton type="text" icon={<UserOutlined />}>
-            {" "}
-            Реєстрація{" "}
+          <CustomButton type='text' icon={<UserOutlined />}>
+            {' '}
+            Реєстрація{' '}
           </CustomButton>
         </Link>
       </Space>
     </Layout.Header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
